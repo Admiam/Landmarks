bf74fcd4ce2ad851aabd776bbee5440e230025c8
Admiam <admiam35@seznam.cz>
2025-07-12 13:39:33 +0200

    adding data

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Adam Míka on 30.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+#Preview {
+    ContentView()
+}
